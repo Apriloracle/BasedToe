68475fae0cf38b88d75806c1d83deff7db713b31
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2024-06-28 18:28:13 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,26 @@ import Image from "next/image";
 import thirdwebIcon from "@public/thirdweb.svg";
 import WalletFuncs from "@/components/WalletFuncs";
 import Attest from "@/components/Attest";
+import TicTacToe from "@/components/TicTacToe";
 
 export default function Home() {
-
   return (
-    <main className="p-4 pb-10 min-h-[100vh] flex items-center justify-center container max-w-screen-lg mx-auto">
-      <div className="py-20">
+    <main className="p-4 pb-10 min-h-[100vh] flex items-center justify-center container max-w-screen-xl mx-auto">
+      <div className="py-20 w-full">
         <Header />
-        <WalletFuncs />
-        <Attest />
+        <div className="flex flex-col md:flex-row gap-8">
+          <div className="w-full md:w-2/3">
+            <WalletFuncs />
+            <Attest />
+          </div>
+          <div className="w-full md:w-1/3">
+            <TicTacToe />
+          </div>
+        </div>
       </div>
     </main>
   );
 }
-
 
 function Header() {
   return (
